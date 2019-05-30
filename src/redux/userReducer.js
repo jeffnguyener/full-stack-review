e3f8791ed5f3e047212b4c3a76443d20eb0787a5
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-30 14:24:23 -0600

    added redux

---
+++ src/redux/userReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    username: '',
+    first_name: '',
+    last_name: '',
+    balance: null,
+    id: null
+}
+
+function reducer(state = initialState, action){
+    return state
+}
+
+export default reducer
